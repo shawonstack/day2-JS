dfeab2582e3d888b78d40b90ebbe0c30e3bf5e3c
shawonstack <shawon934764@gmail.com>
2026-05-13 17:50:57 +0600

    stirng methods finish

--- day2.js
+++ day2.js
@@ -9,5 +9,25 @@ let person = {
 };
 let newPerson = person;
 person.name = 'amin';
-console.log(person);
-console.log(newPerson);
+// console.log(person);
+// console.log(newPerson);
+
+// includes
+let sentence = 'i love javascript';
+let resultSen = sentence.includes('javascript');
+
+//slice
+
+let country = 'bangladesh';
+let rescount = country.slice(0, 5);
+
+// Length
+let stirng = 'he is  a very good boy';
+
+// split
+let name = 'apple, banana, mango';
+let result = name.split(',');
+// console.log(result);
+let a = 'I am learning JS';
+let result2 = a.replace('js', 'javascript');
+console.log(result2);
