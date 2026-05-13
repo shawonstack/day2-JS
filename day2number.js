8fdd75127507a6e3cb4a6c194c772531ca11c545
shawonstack <shawon934764@gmail.com>
2026-05-13 23:47:22 +0600

    added numbber or sum

--- day2number.js
+++ day2number.js
@@ -1,12 +1,28 @@
 let number = 100;
 let result = number.toString();
-console.log(typeof result);
+// console.log(typeof result);
 let price = 1000;
 let res2 = price.toString();
-console.log(res2, 'taka');
+// console.log(res2, 'taka');
 //tofixd--- after decimal number, how many digit store to fixd it
 let numbers = 55;
 
 let number3 = 55 / 2;
 let res3 = number3.toFixed(2);
-console.log(res3);
+// console.log(res3);
+
+let taka = 33.445566;
+let takares = taka.toFixed(2);
+console.log(takares);
+
+//perseInt()---> convert strings number to pure bolean number . its work whn the umber is complte. not decimal
+let num = '50px';
+
+console.log(parseInt(num));
+
+const newNum = '444.45p';
+console.log(parseFloat(newNum));
+
+let he = '10' + 5;
+let nechange = parseInt(10) + 5;
+console.log(nechange);
