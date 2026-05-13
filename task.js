8daa09a950a73ee9c0850b1c9ba550f32e479d0d
shawonstack <shawon934764@gmail.com>
2026-05-14 01:14:28 +0600

    day 2 5 task completed. 10 remining

---
+++ task.js
@@ -0,0 +1,27 @@
+// //task1
+// let name = 'shawon';
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.trim());
+
+// //task2
+// console.log('hello' + 5);
+// console.log(5 + 5 + 'hello');
+
+// //task3
+// const name = 'shawon';
+// const age = 22;
+// console.log(`my name is ${name} and my age is ${age}`);
+
+// // task 4
+// console.log(parseInt('42px')); //42
+// console.log(parseFloat('3.14abc')); //3.14
+// console.log(number('hello')); // number is not defined
+
+// // task 5
+// console.log(NaN === NaN); // false return korbe cz ekhne valuse sathe type of tao ck korche. jehetu valo kono kichu nai
+// // isNaN --> muloto kono value kina ta ck kore jmn
+// console.log(isNaN(10)); // false karon 10 holo number
+// console.log(isNaN('name')); //true cz tring ta number na
