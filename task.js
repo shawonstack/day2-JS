25ddf3f730d961249758697f62d0e4bded3f72e9
shawonstack <shawon934764@gmail.com>
2026-05-14 01:28:19 +0600

    7 complete

--- task.js
+++ task.js
@@ -1,28 +1,36 @@
-// //task1
-// let name = 'shawon';
+//task1
+let name = 'shawon';
 
-// console.log(name.length);
-// console.log(name.toUpperCase());
-// console.log(name.toLowerCase());
-// console.log(name.trim());
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.trim());
 
-// //task2
-// console.log('hello' + 5);
-// console.log(5 + 5 + 'hello');
+//task2
+console.log('hello' + 5);
+console.log(5 + 5 + 'hello');
 
-// //task3
-// const name = 'shawon';
-// const age = 22;
-// console.log(`my name is ${name} and my age is ${age}`);
+//task3
+const name2 = 'shawon';
+const age = 22;
+console.log(`my name is ${name2} and my age is ${age}`);
 
-// // task 4
-// console.log(parseInt('42px')); //42
-// console.log(parseFloat('3.14abc')); //3.14
+// task 4
+console.log(parseInt('42px')); //42
+console.log(parseFloat('3.14abc')); //3.14
 // console.log(number('hello')); // number is not defined
 
-// // task 5
-// console.log(NaN === NaN); // false return korbe cz ekhne valuse sathe type of tao ck korche. jehetu valo kono kichu nai
-// // isNaN --> muloto kono value kina ta ck kore jmn
-// console.log(isNaN(10)); // false karon 10 holo number
+// task 5
+console.log(NaN === NaN); // false return korbe cz ekhne valuse sathe type of tao ck korche. jehetu valo kono kichu nai
+// isNaN --> muloto kono value kina ta ck kore jmn
+console.log(isNaN(10)); // false karon 10 holo number
 // console.log(isNaN('name')); //true cz tring ta number na
-let a = 4;
+
+// task6
+let country = 'Bangladesh';
+console.log(country.slice(2, 5));
+console.log(country.substring(2, 5));
+console.log(country.indexOf(0));
+//task7
+let food = 'apple, banana, mango';
+console.log(food.split(','));
