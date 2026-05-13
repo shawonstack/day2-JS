99e9612e5898e8bf216d921301e7f9f5526caaab
shawonstack <shawon934764@gmail.com>
2026-05-13 16:11:42 +0600

    data types primitive and referrence valuse is clear

---
+++ day2.js
@@ -0,0 +1,13 @@
+// let a = 40;
+// a = 50;
+// let b = a;
+
+// console.log(a);
+// console.log(b);
+let person = {
+  name: 'saidur',
+};
+let newPerson = person;
+person.name = 'amin';
+console.log(person);
+console.log(newPerson);
